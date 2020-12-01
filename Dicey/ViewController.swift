234a8fb1d166ca9251e60af85826ac586b213da4
Saurabh Mirajkar <mirajkar46@gmail.com>
2020-12-01 21:13:43 +0530

    Complete UI design

--- Dicey/ViewController.swift
+++ Dicey/ViewController.swift
@@ -8,9 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var firstDiceImageView: UIImageView!
+    @IBOutlet weak var secondDiceImageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        firstDiceImageView.alpha = 0.5
+        secondDiceImageView.alpha = 0.8
         
     }
 
